896447abeb88612a420823790e83673d972b70aa
X-man34 <caleb.hottes@gmail.com>
2025-04-15 19:35:37 -0600

    Refactor file paths to use pathlib for cross-platform compatibility

--- Question4PartBandC.py
+++ Question4PartBandC.py
@@ -11,20 +11,14 @@ if __name__ == "__main__":
     #Question 4 part b, the triad method, is implemented in the filters file. 
 
 
-    csv_data = pd.read_csv('csv_files\question2_input.csv', header=None,
+    data_path = Path("csv_files") / "question2_input.csv"
+    csv_data = pd.read_csv(str(data_path), header=None,
                     names=['t', 'mx', 'my', 'mz', 'gyrox', 'gyroy', 'gyroz', 'ax', 'ay', 'az'])
-
-    # csv_data = pd.read_csv('csv_files\charlie_phone_data.csv')
-    # csv_data = csv_data.rename(columns={"accelerometerAccelerationX(G)": "ax", "accelerometerAccelerationY(G)": "ay", "accelerometerAccelerationZ(G)": "az", "gyroRotationX(rad/s)": "gyrox", "gyroRotationY(rad/s)": "gyroy", "gyroRotationZ(rad/s)": "gyroz", "magnetometerX(µT)": "mx", "magnetometerY(µT)": "my", "magnetometerZ(µT)": "mz", "accelerometerTimestamp_sinceReboot(s)": "t"})
-    # csv_data[["ax", "ay", "az"]] = csv_data[["ax", "ay", "az"]] * 9.80665# accelerometer data is in G's not m/s^2
-    # csv_data["az"] = csv_data["az"] * -1# Need to flip z axis to match the coord system for this project. 
-
     dt = 0.01
 
     times = []
     rotation_angles = []
-    triad_estimator = MahonyWithTriadEstimator(dt, kP=2)#Custom filter that Caleb made. I originally thought it was what the instructions wanted. 
-    # triad_estimator = TriadEstimator(dt, kP=2)
+    triad_estimator = TriadEstimator(dt, kP=2)
     # Perfrom the simulation and get the data
     times, rotation_angles, _, _, _, _, _ = simulate_and_visualize_data(csv_data, dt, triad_estimator,  do_3D_vis=True, show_body_coords=False, show_extra_vectors=True, show_spatial_coords=True)
 
