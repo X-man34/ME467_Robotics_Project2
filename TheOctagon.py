df1fa666a38ee3e1d385cc5900c7b0eb99acaad5
X-man34 <caleb.hottes@gmail.com>
2025-04-15 22:38:50 -0600

    Update data source and filter parameters; streamline threading for simulation

--- TheOctagon.py
+++ TheOctagon.py
@@ -53,25 +53,27 @@ if __name__ == "__main__":
 
     # Load pre recorded CSV data
     #TODO load data live and have a real time estimate of what is going on. 
-    data_path = Path("csv_files") / "charlie_still_weird.csv"
+    data_path = Path("csv_files") / "charlie_phone_540.csv"
     csv_data = pd.read_csv(str(data_path))
     csv_data = csv_data.rename(columns={"accelerometerAccelerationX(G)": "ax", "accelerometerAccelerationY(G)": "ay", "accelerometerAccelerationZ(G)": "az", "gyroRotationX(rad/s)": "gyrox", "gyroRotationY(rad/s)": "gyroy", "gyroRotationZ(rad/s)": "gyroz", "magnetometerX(µT)": "mx", "magnetometerY(µT)": "my", "magnetometerZ(µT)": "mz", "accelerometerTimestamp_sinceReboot(s)": "t"})
     csv_data[["ax", "ay", "az"]] = csv_data[["ax", "ay", "az"]] * 9.80665# accelerometer data is in G's not m/s^2
     csv_data["az"] = csv_data["az"] * -1# Need to flip z axis to match the coord system for this project. 
     
-    mahony_filter = MahonyFilter(dT=.01, kp=1.0, kI=0.3, ka_nominal=.8, km_nominal=.2)
+    mahony_filter = MahonyFilter(dT=.01, kp=1.0, kI=0.3, ka_nominal=.5, km_nominal=.5)
     triad_estimator = TriadEstimator(dT=.01, kP=2)
     naive_estimator = NaiveEstimator(.01)
     charlie_filter = HybridMahonyTriadFilter(.01, threshold=.1, kp=1.0, kI=0.3, ka_nominal=1.0, km_nominal=.5)
     caleb_filter = MahonyWithTriadEstimator(.01, kP=2)
 
 
-    mahony_thread = threading.Thread(target=simulate_and_visualize_data, args=(csv_data, 0.01, mahony_filter, True, True, True,True))
+
+    # You can play around here and put different filters up. 
+    caleb = threading.Thread(target=simulate_and_visualize_data, args=(csv_data, 0.01, caleb_filter, True, True, True,True))
     
 
-    naieve_thread = threading.Thread(target=simulate_and_visualize_data, args=(csv_data, 0.01, naive_estimator, True, False, True,True))
-    mahony_thread.start()
-    naieve_thread.start()
+    charles = threading.Thread(target=simulate_and_visualize_data, args=(csv_data, 0.01, charlie_filter, True, True, False,True))
+    caleb.start()
+    charles.start()
     # times, rotation_angles, bias_estimates, error_estimates, roll, pitch, yaw = simulate_and_visualize_data(csv_data, .01, mahony_filter, do_3D_vis=True, show_body_coords=True, show_extra_vectors=True, show_spatial_coords=True)
     
     # plot_rotation_data(times, rotation_angles, title_suffix="(Filtered)", data_in_radians=True, convert=False)
